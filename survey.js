43c47ed86b001b10f2fa83949fd2f8f152d06d77
Erica <ericasu33@gmail.com>
2020-11-04 01:22:04 +0000

    refractored code

--- survey.js
+++ survey.js
@@ -5,20 +5,13 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-rl.question("What is your name? : ", (answer) => {
-  const name = answer;
-  rl.question("What is an activity you like doing? : ", (answer) => {
-    const activity = answer;
-    rl.question("What do you listen to while doing that? : ", (answer) => {
-      const music = answer;
-      rl.question("Which meal is your favourite (dinner, brunch...etc)? : ", (answer) => {
-        const meal = answer;
-        rl.question("What is your favourite thing to eat for that meal? : ", (answer) => {
-          const food = answer;
-          rl.question("Which sport is your absolute favourite? : ", (answer) => {
-            const sport = answer;
-            rl.question("What is your superpower? : ", (answer) => {
-              const superpower = answer;
+rl.question("What is your name? : ", (name) => {
+  rl.question("What is an activity you like doing? : ", (activity) => {
+    rl.question("What do you listen to while doing that? : ", (music) => {
+      rl.question("Which meal is your favourite (dinner, brunch...etc)? : ", (meal) => {
+        rl.question("What is your favourite thing to eat for that meal? : ", (food) => {
+          rl.question("Which sport is your absolute favourite? : ", (sport) => {
+            rl.question("What is your superpower? : ", (superpower) => {
               console.log(`${name} loves to ${activity} while listening to ${music}, devouring ${food} for ${meal}, prefers ${sport} and is amazing at ${superpower}`);
               rl.close();
             });
